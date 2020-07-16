4ea484ada9244093616519569a23ed98af7d93ee
yovsky <muthuimbrian@gmail.com>
2020-07-16 14:33:33 +0300

    Add users collection

---
+++ migrations/1594899708879-create-users-collection.js
@@ -0,0 +1,54 @@
+"use strict";
+const { client } = require("../model/connect");
+
+const dbName = process.env.DATABASE_NAME;
+
+module.exports.up = function (next) {
+  client.connect(function (err) {
+    if (err) {
+      client.close();
+      throw err;
+    }
+
+    client
+      .db(dbName)
+      .createCollection("users")
+      .then(() => {
+        console.log("Created users collection");
+        next();
+      })
+      .catch((err) => {
+        throw err;
+      })
+      .finally(() => {
+        client.close();
+      });
+  });
+};
+
+module.exports.down = function (next) {
+  client.connect(function (err) {
+    if (err) {
+      client.close();
+      throw err;
+    }
+
+    client
+      .db(dbName)
+      .dropCollection("users")
+      .then((didDrop) => {
+        if (didDrop) {
+          console.log("Dropped collection users");
+        } else {
+          throw Error("Failed to drop collextion users");
+        }
+        next();
+      })
+      .catch((err) => {
+        throw err;
+      })
+      .finally(() => {
+        client.close();
+      });
+  });
+};
