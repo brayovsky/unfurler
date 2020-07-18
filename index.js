873e1f8afc9da9e0ba8f1811d1e896aac1f855c6
yovsky <muthuimbrian@gmail.com>
2020-07-18 18:23:20 +0300

    Add upload endpoint

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const busboy = require('connect-busboy');
 const paths = require('./lib/paths');
 const config = require('./config');
 const authenticate = require('./lib/users/authenticate');
 const parseUrl = require('./lib/urls/parse');
 const translate = require('./lib/urls/translate');
+const upload = require('./lib/files/upload');
 const { client } = require('./model/client');
 
 const app = express();
+
 app.use(express.json());
+app.use(busboy());
 
 const db = process.env.DATABASE_NAME;
 
@@ -23,5 +27,9 @@ app.post(paths.login, (req, res) => {
 app.get(paths.parseUrl, parseUrl);
 
 app.post(paths.translate, translate);
+
+app.post(paths.upload, (req, res) => {
+  upload(req, res, client, db);
+});
 
 app.listen(config.port, () => console.log(`listening on port ${config.port}`));
