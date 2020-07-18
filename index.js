47ac71a43ffdbd5ec91e79f165428b055a513e26
yovsky <muthuimbrian@gmail.com>
2020-07-18 15:31:13 +0300

    Add translate function

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const paths = require('./lib/paths');
 const config = require('./config');
 const authenticate = require('./lib/users/authenticate');
 const parseUrl = require('./lib/urls/parse');
+const translate = require('./lib/urls/translate');
 const { client } = require('./model/client');
 
 const app = express();
+app.use(express.json());
+
 const db = process.env.DATABASE_NAME;
 
 app.get(paths.root, (req, res) => {
@@ -18,5 +21,7 @@ app.post(paths.login, (req, res) => {
 });
 
 app.get(paths.parseUrl, parseUrl);
+
+app.post(paths.translate, translate);
 
 app.listen(config.port, () => console.log(`listening on port ${config.port}`));
